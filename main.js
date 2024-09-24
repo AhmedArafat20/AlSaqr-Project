f2b5adf703c78fc925f5735ab9a2de5389c5afec
mahmoud alsoudy <modypc8@gmail.com>
2024-09-24 11:55:55 +0300

    finish the index page

--- main.js
+++ main.js
@@ -62,15 +62,15 @@ function mcShow(){
 
 // index page
 let btn = document.getElementById("more");
-let content = [document.getElementById("second"), document.getElementById("third"), document.getElementById("fourth")]
-let cntr = 0
+let content = [document.getElementById("second"), document.getElementById("third"), document.getElementById("fourth")];
+let cntr = 0;
 function clickx(){
   if (cntr == 2)
-    btn.innerHTML="رؤيه كل المشاريع"
-  else if (cntr == 3){
-    btn.setAttribute("href" , "Projects.html")
-  }
+    btn.innerHTML="رؤيه كل المشاريع";
+
+  else if (cntr == 3)
+    btn.setAttribute("href" , "Projects.html");
+
   content[cntr].style.display="block";
-  cntr+=1
-  console.log(cntr)
+  cntr+=1;
 }
